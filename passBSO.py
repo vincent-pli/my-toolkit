fb45177a922d16342c5b8ebb9bd3b0a7e25427d2
vincent-pli <justdoit.pli@gmail.com>
2017-12-22 23:11:34 +0800

    Enhance the symbol which means please input the user name

--- passBSO.py
+++ passBSO.py
@@ -4,11 +4,12 @@ import telnetlib
 HOSTS = ("192.168.1.139")
 user = "foo"
 password = "letmein"
+user_input_symbol=("Username: ", "login: ")
 
 for item in HOSTS:
     tn = telnetlib.Telnet(item)
     tn.set_debuglevel(9)
-    tn.read_until("login: ")
+    tn.expect(user_input_symbol)
     tn.write(user + "\n")
     if password:
         tn.read_until("Password: ")
