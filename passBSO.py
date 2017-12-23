dca9cb566b6f9ad73550b359861f3aa26dd68bef
vincent-pli <justdoit.pli@gmail.com>
2017-12-23 17:36:26 +0800

    Catch the 'Connection refuse' exception, treat it as passed

--- passBSO.py
+++ passBSO.py
@@ -1,20 +1,24 @@
 import sys
 import telnetlib
+import socket
 
-HOSTS = ("192.168.1.139")
-user = "foo"
-password = "letmein"
+HOSTS = ("192.168.3.111")
+user = "foo@163.com"
+password = "xxxxxx"
 user_input_symbol=["Username: ", "login: "]
-
 for item in HOSTS:
-    tn = telnetlib.Telnet(item)
-    tn.set_debuglevel(9)
-    tn.expect(user_input_symbol)
-    tn.write(user + "\n")
-    if password:
-        tn.read_until("Password: ")
-        tn.write(password + "\n")
+    try:
+        tn = telnetlib.Telnet(item)
+        tn.set_debuglevel(9)
+        #tn.read_until("Username: ")
+        tn.expect(user_input_symbol)
+        tn.write(user + "\n")
+        if password:
+            tn.read_until("Password: ")
+            tn.write(password + "\n")
 
-    #tn.write("ls\n")
-    tn.write("exit\n")
-    print tn.read_all()
+        #tn.write("ls\n")
+        tn.write("exit\n")
+        print tn.read_all()
+    except socket.error, e:
+        print item + " BSO has been passed"
