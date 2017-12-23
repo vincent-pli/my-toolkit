eaeaee8f36dec0e7b2094743f4f96b8fc5d22f43
vincent-pli <justdoit.pli@gmail.com>
2017-12-23 22:51:42 +0800

    Add timeout to the connection

--- passBSO.py
+++ passBSO.py
@@ -5,13 +5,14 @@ import socket
 HOSTS = ("192.168.3.111")
 user = "foo@163.com"
 password = "xxxxxx"
-user_input_symbol=["Username: ", "login: "]
+user_input_symbol = ["Username: ", "login: "]
+time_out_sec = 60
 for item in HOSTS:
     try:
-        tn = telnetlib.Telnet(item)
+        tn = telnetlib.Telnet(item, time_out_sec)
         tn.set_debuglevel(9)
         #tn.read_until("Username: ")
-        tn.expect(user_input_symbol)
+        tn.expect(user_input_symbol, time_out_sec)
         tn.write(user + "\n")
         if password:
             tn.read_until("Password: ")
